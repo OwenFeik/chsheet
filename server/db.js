993382649a58c37065da5319061201fa8ac62eee
OwenFeik <owenhfeik@gmail.com>
2021-09-27 23:32:30 +1000

    start thinking about server side

--- server/db.js
+++ server/db.js
@@ -4,11 +4,15 @@
  * through a health check dependency.
  */
 
+const crypto = require("crypto");
 const path = require("path");
+
 const postgres = require("postgres");
 
+/* Contains database schema. */
+const SCHEMA_FILE = path.join(__dirname, "schema.sql");
 
-/* Connect to the Postgres instance, in other container */
+/* Postgres instance, in other container */
 const POSTGRES_URL = (
     "postgresql://"
     + process.env.POSTGRES_USER
@@ -20,20 +24,38 @@ const POSTGRES_URL = (
     + process.env.POSTGRES_DB
 );
 
-console.log("Connecting to", POSTGRES_URL);
-const sql = postgres(POSTGRES_URL);
+const HASH_SALT_LENGTH = 16;
+const HASH_ITERATIONS = 1024;
+const HASH_KEYLEN = 64;
+const HASH_TYPE = "blake2b512";
 
-/* Run schema.sql to ensure that it's up to date. */
-const SCHEMA_FILE = path.join(__dirname, "schema.sql");
-(
-    async () => {
-        console.log("Initialising database schema.")
-        await sql.file(SCHEMA_FILE);
-    }
-)().then(
-    console.log("Initialised database schema.")
-)
+const sql;
 
-/* Functions exposed for access from importing files. */
-const expose = {};
-module.exports = expose;
+const init = () => {
+    console.log("Connecting to", POSTGRES_URL);
+    sql = postgres(POSTGRES_URL);
+    
+    (
+        async () => {
+            console.log("Initialising database schema.")
+            await sql.file(SCHEMA_FILE);
+        }
+    )().then(
+        console.log("Initialised database schema.")
+    )   
+};
+exports.init = init;
+
+const create_salt = () => {
+    return crypto.randomBytes(HASH_SALT_LENGTH).toString('hex');
+};
+
+const hash_password = (plaintext, salt) => {
+    return crypto.pbkdf2Sync(
+        plaintext,
+        salt,
+        HASH_ITERATIONS,
+        HASH_KEYLEN,
+        HASH_TYPE
+    ).toString('hex');
+};
--- server/schema.sql
+++ server/schema.sql
@@ -1,4 +1,12 @@
-CREATE TABLE IF NOT EXISTS mytable (
+CREATE TABLE IF NOT EXISTS sheets (
     id INTEGER PRIMARY KEY,
-    val VARCHAR(64)
+    sheet JSON,
+    updated INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    username VARCHAR(32),
+    salt VARCHAR(32),
+    hashed_password VARCHAR(128)
 );
